6d2a7f227fd41736084b82f9e190760f89c7af52
herdeybayor <shereefdeen4real@gmail.com>
2022-04-21 21:07:37 +0100

    contact complete

--- components/Contact/ContactInfo.tsx
+++ components/Contact/ContactInfo.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import ContactLayout from './ContactLayout'
+import { BiUserPin } from 'react-icons/bi'
+import { RiUserLocationLine } from 'react-icons/ri'
+import { MdAlternateEmail } from 'react-icons/md'
 
 const ContactInfo = () => {
   return (
     <div className="contact__container col-span-4 rounded-2xl bg-creamy px-5 py-10 sm:rounded-[3rem] sm:py-16 sm:px-10">
       <h1 className="mb-5 text-center text-xl font-semibold">Contact Info</h1>
+      <div className="flex flex-col space-y-5">
+        <ContactLayout title="Name" subtitle="Adebayo Sherifdeen">
+          <BiUserPin />
+        </ContactLayout>
+        <ContactLayout title="Location" subtitle="Lagos, Nigeria.">
+          <RiUserLocationLine />
+        </ContactLayout>
+        <ContactLayout title="Email" subtitle="herdeybayor4real@gmail.com">
+          <MdAlternateEmail />
+        </ContactLayout>
+      </div>
     </div>
   )
 }
--- components/Contact/ContactLayout.tsx
+++ components/Contact/ContactLayout.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-const ContactLayout = () => {
-  return <div>ContactLayout</div>
+interface Props {
+  title: string
+  subtitle: string
+}
+
+const ContactLayout: React.FC<Props> = ({ children, title, subtitle }) => {
+  return (
+    <div className="flex items-center space-x-5 text-gray-400">
+      <div className="text-6xl md:text-7xl lg:text-8xl">{children}</div>
+      <div>
+        <h1 className="text-xl font-semibold">{title}</h1>
+        <p className="break-all text-lg">{subtitle}</p>
+      </div>
+    </div>
+  )
 }
 
 export default ContactLayout
