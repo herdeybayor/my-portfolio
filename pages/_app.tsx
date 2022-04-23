bbb1e02055e39a028da0bcc70714c1214a2d9204
herdeybayor <shereefdeen4real@gmail.com>
2022-04-23 22:05:15 +0100

    page animation implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect, useState } from 'react'
+import { Router } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [siteViews, setSiteViews] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const removeLoading = () => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }
+
   useEffect(() => {
+    removeLoading()
+    Router.events.on('routeChangeStart', () => setIsLoading(true))
+    Router.events.on('routeChangeComplete', () => removeLoading())
+    Router.events.on('routeChangeError', () => removeLoading())
+
+    //Fetch Page View
     const fetchPageViews = async () => {
       const res = await fetch(
         'https://api.countapi.xyz/hit/herifdeenadebayo.com'
@@ -14,7 +29,16 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
     fetchPageViews()
   }, [])
-  return <Component siteViews={siteViews} {...pageProps} />
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="page-switch-animation" />
+      ) : (
+        <Component siteViews={siteViews} {...pageProps} />
+      )}
+    </>
+  )
 }
 
 export default MyApp
