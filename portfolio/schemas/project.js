a789ec5b56e62e18079ed6612ed1cfd1239673e0
herdeybayor <shereefdeen4real@gmail.com>
2023-11-26 12:23:02 +0100

    feat: add display field to schema 🎉

--- portfolio/schemas/project.js
+++ portfolio/schemas/project.js
@@ -1,35 +1,41 @@
 export default {
-  name: 'project',
-  title: 'Project',
-  type: 'document',
-  fields: [
-    {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    },
-    {
-      name: 'subtitle',
-      title: 'Subtitle',
-      type: 'string',
-    },
-    {
-      name: 'frameworks',
-      title: 'Frameworks',
-      type: 'string',
-    },
-    {
-      name: 'url',
-      title: 'Url',
-      type: 'string',
-    },
-    {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-  ],
+    name: 'project',
+    title: 'Project',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'subtitle',
+            title: 'Subtitle',
+            type: 'string',
+        },
+        {
+            name: 'frameworks',
+            title: 'Frameworks',
+            type: 'string',
+        },
+        {
+            name: 'url',
+            title: 'Url',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'display',
+            title: 'Display',
+            type: 'boolean',
+            default: true,
+        }
+    ],
 }
